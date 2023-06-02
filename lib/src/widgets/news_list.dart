3f2ed00582a33c7a89067a5fbda98c7657cf798d
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-06-02 16:02:41 -0600

    295. Diseño del widget de las noticias

--- lib/src/widgets/news_list.dart
+++ lib/src/widgets/news_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/models/news_models.dart';
+import 'package:news_app/src/theme/theme.dart';
 
 class NewsList extends StatelessWidget {
   final List<Article> news;
@@ -11,7 +12,88 @@ class NewsList extends StatelessWidget {
     return ListView.builder(
         itemCount: news.length,
         itemBuilder: (_, int index) {
-          return Text(news[index].title);
+          return _News(article: news[index], index: index);
         });
+  }
+}
+
+class _News extends StatelessWidget {
+  final Article article;
+  final int index;
+
+  const _News({
+    required this.article,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CardTopBar(
+          article: article,
+          index: index,
+        ),
+        _CardTitle(article: article),
+        _CardImage(article: article)
+      ],
+    );
+  }
+}
+
+class _CardTopBar extends StatelessWidget {
+  final Article article;
+  final int index;
+
+  const _CardTopBar({
+    required this.article,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text(
+            '${index + 1}',
+            style: TextStyle(color: myTheme.colorScheme.secondary),
+          ),
+          Text(
+            article.source.name,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardTitle extends StatelessWidget {
+  final Article article;
+
+  const _CardTitle({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        article.title,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class _CardImage extends StatelessWidget {
+  final Article article;
+
+  const _CardImage({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('HOla mundo');
   }
 }
